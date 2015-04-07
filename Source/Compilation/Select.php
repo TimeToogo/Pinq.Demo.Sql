6f610ed4ebab2611621d1b74ea2c067235b621dc
Elliot Levin <elliotlevin@hotmail.com>
2015-04-07 21:43:27 +1000

    Fix docblock

--- Source/Compilation/Select.php
+++ Source/Compilation/Select.php
@@ -29,7 +29,7 @@ class Select extends Query implements IRequestCompilation
     }
 
     /**
-     * @return Select
+     * @return void
      */
     public function wrapSelectAsDerivedTable()
     {
